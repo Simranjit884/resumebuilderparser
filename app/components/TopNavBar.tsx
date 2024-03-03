d41c817d99b5987162334fede2a6d5164186307f
samy884 <ssgill884@gmail.com>
2024-03-03 21:28:09 +0530

    topnavbar header

--- app/components/TopNavBar.tsx
+++ app/components/TopNavBar.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { cx } from "../lib/cx";
 
 function TopNavBar() {
   const pathName = usePathname();
   const isHomePage = pathName === "/";
 
-  return <header aria-label="Site Header"></header>;
+  return (
+    <header
+      aria-label="Site Header"
+      className={cx(
+        "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-100 px-3 lg:px-12",
+        isHomePage && "bg-dot"
+      )}
+    ></header>
+  );
 }
 
 export default TopNavBar;
