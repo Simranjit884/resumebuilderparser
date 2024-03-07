eda117734b502768b2fb64d207ed69c418d86c2c
samy884 <ssgill884@gmail.com>
2024-03-07 20:02:58 +0530

    hero section

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -1,1 +1,9 @@
-export const Hero = () => {};
+import { FlexboxSpacer } from "../components/FlexboxSpacer";
+
+export const Hero = () => {
+  return (
+    <section className="lg:flex lg:h-[825px] lg:justify-center">
+      <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:bloxk" />
+    </section>
+  );
+};
