0fe3feba00abea00a5ebc342265851f143a95443
samy884 <ssgill884@gmail.com>
2024-03-07 23:14:36 +0530

    layout and styling

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -4,7 +4,7 @@ import { FlexboxSpacer } from "../components/FlexboxSpacer";
 export const Hero = () => {
   return (
     <section className="lg:flex lg:h-[825px] lg:justify-center">
-      <FlexboxSpacer maxWidth={75} minWidth={50} className="hidden lg:block" />
+      <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:block" />
       <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
         <h1 className="text-primary pb-2 text-4xl font-bold lg:text-5xl">
           Create a professional
